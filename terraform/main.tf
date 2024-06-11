43a9d279f4be5f1b3e373c966996f47db996cd71
Alfonso <alfonso.balmori@skydropx.com>
2024-06-11 10:39:09 -0600

    initial commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -71,7 +71,7 @@ resource "aws_ecr_repository" "app_repo" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-12345678"  # Reemplazar con una AMI válida para us-east-1
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.main.id
   vpc_security_group_ids = [aws_security_group.web.id]
