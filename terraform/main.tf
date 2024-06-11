3ff3e1ff3614e508348a2ba4c392944586e15803
Alfonso <alfonso.balmori@skydropx.com>
2024-06-11 10:28:46 -0600

    initial commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "main" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1"
+  availability_zone = "us-east-1a"
 }
 
 resource "aws_security_group" "web" {
